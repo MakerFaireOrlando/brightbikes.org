c1729bbcdf665550728b5d44f332e0f761122aef
Devusol <dev.dusol@gmail.com>
2021-05-17 20:54:48 -0400

    built out full screen modal for Youtube videos

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -206,3 +206,31 @@ requestAnimationFrame(render);
 
 })(jQuery); // End of use strict
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var vidFrame = document.getElementById("videoFrame");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function videoModal (videoURL){
+  console.log(videoURL);
+  console.log(vidFrame);
+  vidFrame.setAttribute("src", videoURL);
+  modal.style.display = "block";
+}
